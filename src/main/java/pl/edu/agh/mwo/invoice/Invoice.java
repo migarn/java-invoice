a9a832ccbff2d9bac1db11c4bdd08d741422dbef
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-02-01 19:52:08 +0100

    Version from lab cleaned up

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,43 +3,40 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
-import com.sun.javafx.collections.MappingChange.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Map<Product, Integer> products = new HashMap<Product, Integer>();
+	private Map<Product, Integer> products = new HashMap<>();
 
 	public void addProduct(Product product) {
 		addProduct(product, 1);;
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		products.put(product, quantity);
+		
 	}
 
 	public BigDecimal getSubtotal() {
-		if (products == null) {
+		if (products.isEmpty()) {
 			return BigDecimal.ZERO;
 		}
-		BigDecimal subtotal = new BigDecimal.ZERO;
-		for (Product product : products.) {
-			
-		}
+		return null;
 
 	}
 
 	public BigDecimal getTax() {
-		if (products == null) {
+		if (products.isEmpty()) {
 			return BigDecimal.ZERO;
 		}
 		return null;
 	}
 
 	public BigDecimal getTotal() {
-		if (products == null) {
+		if (products.isEmpty()) {
 			return BigDecimal.ZERO;
 		}
 		return null;
