95f60fd5da7dc9f13cdfac1f5182804d80614928
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-12 19:23:19 +0200

    Style corrected once again

--- src/main/java/pl/edu/agh/mwo/invoice/InvoicesList.java
+++ src/main/java/pl/edu/agh/mwo/invoice/InvoicesList.java
@@ -7,14 +7,13 @@ import java.util.List;
 public class InvoicesList {
     /* Tu klasa zaimplementowana za pomocą listy przechowującej numery w pamięci.
      * W rzeczywistości lista raczej byłaby przechowywana w bazie danych.
-     * Istotne jest poprawne działanie metody getNewNumber().	 
+     * Istotne jest poprawne działanie metody getNewNumber().
      */
     private List<Integer> numbers = new ArrayList<Integer>(Arrays.asList(0));
-	
+
     public int getNewNumber() {
         int newNumber = this.numbers.get(this.numbers.size() - 1) + 1;
         this.numbers.add(newNumber);
         return newNumber;
-	}
-
+    }
 }
